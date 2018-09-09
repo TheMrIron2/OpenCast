35a410d35ed8484962c22d6a612c2edfe726ff9c
TheMrIron2 <dwelch.appleid@gmail.com>
2018-09-09 12:43:23 +0100

    CastSDK
    Keeps the same base as FireCast's, but cleaned up.

--- sdk/castsdk.lua
+++ sdk/castsdk.lua
@@ -1,15 +1,18 @@
 term.setBackgroundColour(colours.black)
-term.setTextColor(colors.white)
+term.setTextColour(colours.white)
 term.clear()
 term.setCursorPos(1,1)
 print("Loading APIs...")
 
-local apis = fs.list("FireBox/apis")
+local function loadModules()
+local apis = fs.list("/OpenCast/Modules/")
 for k, v in pairs(apis) do
-	os.loadAPI("FireBox/apis/"..v)
+	os.loadAPI("OpenCast/Modules/"..v)
 	print("Loaded "..v)
 end
+end
 
+loadModules()
 print("Insert a disk to use for development")
 
 local e, par = os.pullEvent("disk")
@@ -17,21 +20,28 @@ if not disk.hasData(par) then
 	print("Invalid disk!")
 	return
 end
-sleep(0.1)
+sleep()
 term.clear()
+
 term.setCursorPos(1,1)
-print("FireCast IDE [SDK]")
+print("OpenCast SDK (CastSDK)")
+
 write("Name of the game: ")
 local name = read()
+
 write("Version: ")
 local version = read()
+
 write("Author: ")
 local author = read()
+
 write("Name of the main file to run on disk\n(EXAMPLE: mygame): ")
 local run = read()
+
 local f = fs.open(disk.getMountPath(par).."/firecastlaunch","w")
 f.write("run = \""..run.."\"\ngameName = \""..name.."\"\nversionGame = \""..version.."\"\nauthorGame = \""..author.."\"\n")
 f.close()
+
 disk.setLabel(par, name.." [FireCast]")
 shell.run("/rom/programs/edit", disk.getMountPath(par).."/"..run)
 local f = fs.open(disk.getMountPath(par).."/start","w")
